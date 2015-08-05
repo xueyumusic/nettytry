513e77a440c4aeb552c2c4ab709b7b522435d517
xueyu <xueyua@chanjet.com>
2015-08-05 10:59:35 +0800

    init urlmapper

--- src/main/java/org/xueyu/conf/UrlMapper.java
+++ src/main/java/org/xueyu/conf/UrlMapper.java
@@ -10,8 +10,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class UrlMapper {
 	private static Map<String, Object> urlmapper = new HashMap<String, Object>();
+	private static Map<String, Method> methodMapper = new HashMap<String, Method>();
 	static {
 		urlmapper.put("goo", "org.xueyu.rmi.GooFunc.func");
+		
+		initUrlMapper();
+	}
+	
+	private static void initUrlMapper() {
+		for (Map.Entry<String, Object> entry: urlmapper.entrySet()) {
+			//String value = (String)entry.getValue();
+			String name = (String)urlmapper.get(entry.getKey());
+			int sep = name.lastIndexOf('.');
+			String fullclassname = name.substring(0, sep);
+			String funcname = name.substring(sep+1);
+			try {
+				System.out.println("##full:"+fullclassname+"  funcname:"+funcname);
+				Class<?> cs = Class.forName(fullclassname);
+				Method method = cs.getMethod(funcname, null);
+				//Object obj = method.invoke(null);
+				methodMapper.put(entry.getKey(), method);
+			} catch (Exception ex) {
+				
+			}
+		}
 	}
 	
 	public static Object findAndInvokeFunc(String reqname) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
